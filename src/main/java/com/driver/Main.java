ef89f9c6532bdb773daa5a35dd05d08a5c326702
Ritwik <ritwikhero@gmail.com>
2023-05-11 14:43:12 +0530

    nested class product created

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
 
+        int result1 = p.product(2,3);
+        System.out.println(result1);
+    }
+    public static class Product {
+//        int x;
+//        int y;
+
+        public int product(int x, int y) {
+           return x*y;
+        }
+    }
 }
