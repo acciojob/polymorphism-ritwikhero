e9d70e423cc19e3815a00111d0a5c2bf60abdf66
Ritwik <ritwikhero@gmail.com>
2023-05-11 14:47:35 +0530

    overloading complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,6 +6,12 @@ public class Main {
 
         int result1 = p.product(2,3);
         System.out.println(result1);
+
+        int result2 = p.product(2,3,4);
+        System.out.println(result2);
+
+        double result3 = p.product(6.9,9.6);
+        System.out.println(result3);
     }
     public static class Product {
 //        int x;
@@ -13,6 +19,14 @@ public class Main {
 
         public int product(int x, int y) {
            return x*y;
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return x*y;
         }
     }
 }
